d25d584a74170a070202b3ceddb6401fafc167a9
Syahroni12 <ronixria@gmail.com>
2021-01-26 09:23:04 +0700

    first commite

---
+++ app/views/about/index.php
@@ -0,0 +1,7 @@
+
+    <div class="container">
+    <h1>my About</h1>
+    <img src="<?= BASEURL;?>/img/RONI.JPG" alt="Muhammad syahroni"width="200" class="rounded-circle shadow">
+    <p>Halo,nama saya <?=$data['nama'];?>,umur saya <?= $data['umur']; ?>,
+    saya adalah seorang <?=$data['pekerjaan']; ?>.</p> 
+</div>
